6c3af711944314bea69a17423298f6bc03771b68
三咲智子 <sxzz@sxzz.moe>
2022-03-26 01:27:49 +0800

    chore: drop node 12 support

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['./src/index.ts'],
   format: ['esm'],
-  target: 'node12',
+  target: 'node14.17',
   clean: true,
   platform: 'node',
 })
