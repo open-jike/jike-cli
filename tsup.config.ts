91623af6567d462d539aadc0eb6c640a4aaf178f
三咲智子 <sxzz@sxzz.moe>
2022-05-02 20:36:35 +0800

    chore: upgrade node version

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['./src/index.ts'],
   format: ['esm'],
-  target: 'node14.17',
+  target: 'node14.19',
   clean: true,
   platform: 'node',
 })
